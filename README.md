1d0371cb39031158d22af7699c0ca49a51b7f4aa
Stanislav Sheinoha <stanyslav.sheinoha@gmail.com>
2023-02-16 08:41:44 +0200

    Adds some files:
    - installs "npm i prop-types";
    - creates component Section.jsx;
    - edits App.jsx
    - edits readme file.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# goit-react-hw-02-feedback
+goit-react-hw-01 ==> Events and status. Forms (part 1 - feedback)
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,4 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import React from 'react';
+
+import { Section } from './Section/Section';
